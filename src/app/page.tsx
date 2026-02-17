de2fb30676e2366634c7c68304c7a8084eed2868
R Amarthya Sreechand <amarthyasreechand@gmail.com>
2026-02-18 01:18:53 +0530

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,36 @@ import { redirect } from "next/navigation";
 import { auth } from "~/server/better-auth";
 import { getSession } from "~/server/better-auth/server";
 
-export default async function Home() {
-  const session = await getSession();
+const mockUrls = [
+    "https://koatqvtbno.ufs.sh/f/PrJRq7EsZuIM7aNIqKVHr02nUPSBtiZwjlpCyVJKxu8zNfkM",
+    "https://koatqvtbno.ufs.sh/f/PrJRq7EsZuIMyLtzUXVr5L9Ie4ENFz2nGMYJBQfk1cSXuDPo",
+    "https://koatqvtbno.ufs.sh/f/PrJRq7EsZuIMinuqBToIC93LySf7XmJwKRaE0gVNYporZDQW",
+    "https://koatqvtbno.ufs.sh/f/PrJRq7EsZuIMqk0BjP6gSCkWr1OpsGZHz3X07u8VybL5FxvU",
+    "https://koatqvtbno.ufs.sh/f/PrJRq7EsZuIMf0AAtxRAQSW2FEl3UxfedLJoGpD7zh5kPmv9",
+    "https://koatqvtbno.ufs.sh/f/PrJRq7EsZuIMFJBhuGQC42fn7mwr8VQ5tclbpP1ZDHj6kXe0",
+]
+const mockImgs = mockUrls.map((url, i) => ({
+    i: i + 1,
+    url
+}))
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    Hello bookmarks in progress
-    </main>
-  );
+export default async function Home() {
+    const session = await getSession();
+
+
+
+    return (
+        <main className="">
+            <div className="flex flex-wrap justify-center gap-4">
+                {
+                    [...mockImgs, ...mockImgs, ...mockImgs].map((img, i) => (
+                        <div key={i} className="w-48 h-48">
+                            <img src={img.url} alt="image" />
+                        </div>
+                    ))
+                }
+            </div>
+            Hello bookmarks in progress
+        </main>
+    );
 }
